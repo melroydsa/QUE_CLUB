8d5b3a3a105cbda6caa4d52f54a89c63d7c788f8
paranoid147 <dsamelroy5@gmail.com>
2023-07-15 23:55:02 +0530

    Changed Video directory

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <>
         <video id="video-bg" autoPlay muted loop>
-            <source src="/Videos/Vid.mp4" type="video/mp4" />
+            <source src="../../Videos/Vid.mp4" type="video/mp4" />
         </video>
         <Hero MainTextOut="Queue" MainTextOth="Club" SubText="Escape to a World of Refined Leisure: Discover the Perfect Shot at the Queue Club" MainTextSize="200px" MainTextMarginTop="150px" SubTextMarginTop="320px"/>
         <div className='parent'>
